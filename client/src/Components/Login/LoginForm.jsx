21389f855ce81f013c1309784344be3c980fdedf
Hain Thit <hainlthit@gmail.com>
2022-03-14 18:48:49 -0400

    typo

--- client/src/Components/Login/LoginForm.jsx
+++ client/src/Components/Login/LoginForm.jsx
@@ -27,7 +27,7 @@ function LoginForm({onLogin}) {
     
     return (
         <Container>
-            <h1>Inventory & POS</h1>
+            <h1>LOG IN FORM BABY</h1>
             <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3">
                     <Form.Label>Email address</Form.Label>

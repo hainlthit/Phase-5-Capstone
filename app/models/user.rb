7c1c3b4a244c2be52c655438cfc918cefee97787
Hain Thit <hainlthit@gmail.com>
2022-03-15 19:24:52 -0400

    carousel

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
     has_many :received_follows, foreign_key: :followed_user_id, class_name: "Follow"
     has_many :followers, through: :received_follows, source: :follower
 
+    has_many :given_follows, foreign_key: :follower_id, class_name: "Follow"
+    has_many :followings, through: :given_follows, source: :followed_user
+
     has_secure_password
     
     validates :username, presence: true, uniqueness: true
--- client/src/Components/Villagers/VillagerCard.jsx
+++ client/src/Components/Villagers/VillagerCard.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
-import { Button } from 'react-bootstrap';
+import { Button } from "react-bootstrap";
 import { Carousel } from "react-bootstrap";
 
-
 export default function VillagerCard() {
-
   const [flipCard, setFlipCard] = useState(true);
 
   function flipHandler() {
@@ -15,61 +13,131 @@ export default function VillagerCard() {
     {
       id: 1,
       image: "https://acnhapi.com/v1/icons/villagers/1",
-      name: "fake review",
-      personality: "john doe"
-  
+      image2: "https://acnhapi.com/v1/images/villagers/1",
+      name: "Cyrano",
+      personality: "Cranky",
+      birthday: "9/3",
+      species: "Anteater",
     },
     {
       id: 2,
-      image: "https://acnhapi.com/v1/icons/villagers/1",
-      content: "fake review",
-      author: "jane doe"
+      image: "https://acnhapi.com/v1/icons/villagers/2",
+      image2: "https://acnhapi.com/v1/images/villagers/2",
+      name: "Bobby",
+      personality: "Nice",
+      birthday: "9/4",
+      species: "Anteater",
     },
     {
       id: 3,
-      image: "https://acnhapi.com/v1/icons/villagers/1",
-      content: "fake review",
-      author: "dane doe"
-    }
-  ]
+      image: "https://acnhapi.com/v1/icons/villagers/3",
+      image2: "https://acnhapi.com/v1/images/villagers/3",
+      name: "Tina",
+      personality: "Cranky",
+      birthday: "9/3",
+      species: "Anteater",
+    },
+  ];
 
   return (
     <>
       <h1 className="reviews-h1">Carousel Test</h1>
-      {flipCard ? (
-        <div class="card">
-          <img
-            class="card-img-top"
-            src="https://acnhapi.com/v1/icons/villagers/1"
-            alt="Cyrano"
-          ></img>
-          <div class="card-body">
-            <h5 class="card-title">Cyrano</h5>
-          </div>
-          <div class="card-body">
-            <Button onClick={flipHandler} aria-pressed="false" >See more details</Button>
-          </div>
-        </div>
-      ) : (
-        <div  class="card">
-          <img
-            class="card-img-top"
-            src="https://acnhapi.com/v1/images/villagers/1"
-            alt="Cyrano"
-          ></img>
-          <div class="card-body">
-            <h5 class="card-title">Cyrano</h5>
-          </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">birthday: 9/3</li>
-            <li class="list-group-item">species: Anteater</li>
-            <li class="list-group-item">personality: Cranky</li>
-          </ul>
-          <div class="card-body">
-            <Button aria-pressed="false" onClick={flipHandler} >Return</Button>
-          </div>
-        </div>
-      )}
+      <Carousel onClick={flipHandler}>
+        {animals.map((animal) => (
+          <Carousel.Item key={animal.id}>
+            {flipCard ? (
+              <div class="card">
+                <img class="card-img-top" src={animal.image} alt="Cyrano"></img>
+                <div class="card-body">
+                  <h5 class="card-title" style={{ textAlign: "center" }}>
+                    Cyrano
+                  </h5>
+                  <div class="card-body" style={{ textAlign: "center" }}>
+                    <Button onClick={flipHandler} aria-pressed="false">
+                      See more details
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div class="card">
+                <img
+                  class="card-img-top"
+                  src={animal.image2}
+                  alt="Cyrano"
+                ></img>
+                <div class="card-body">
+                  <h5 class="card-title" style={{ textAlign: "center" }}>
+                    {animal.name}
+                  </h5>
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item" style={{ textAlign: "center" }}>
+                    Birthday: {animal.birthday}
+                  </li>
+                  <li class="list-group-item" style={{ textAlign: "center" }}>
+                    Dpecies: {animal.species}
+                  </li>
+                  <li class="list-group-item" style={{ textAlign: "center" }}>
+                    Personality: {animal.personality}
+                  </li>
+                  <li class="list-group-item" style={{ textAlign: "center" }}>
+                    <Button
+                      onClick={flipHandler}
+                      aria-pressed="false"
+                      style={{ textAlign: "center" }}
+                    >
+                      See less
+                    </Button>
+                  </li>
+                  <li
+                    class="list-group-item"
+                    style={{ textAlign: "center" }}
+                  ></li>
+                </ul>
+              </div>
+            )}
+          </Carousel.Item>
+        ))}
+      </Carousel>
     </>
   );
+}
+
+{
+  /* <h1 className="reviews-h1">Carousel Test</h1>
+{flipCard ? (
+  <div class="card">
+    <img
+      class="card-img-top"
+      src="https://acnhapi.com/v1/icons/villagers/1"
+      alt="Cyrano"
+    ></img>
+    <div class="card-body">
+      <h5 class="card-title">Cyrano</h5>
+    </div>
+    <div class="card-body">
+      <Button onClick={flipHandler} aria-pressed="false" >See more details</Button>
+    </div>
+  </div>
+) : (
+  <div  class="card">
+    <img
+      class="card-img-top"
+      src="https://acnhapi.com/v1/images/villagers/1"
+      alt="Cyrano"
+    ></img>
+    <div class="card-body">
+      <h5 class="card-title">Cyrano</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">birthday: 9/3</li>
+      <li class="list-group-item">species: Anteater</li>
+      <li class="list-group-item">personality: Cranky</li>
+    </ul>
+    <div class="card-body">
+      <Button aria-pressed="false" onClick={flipHandler} >Return</Button>
+    </div>
+  </div>
+)} */
 }

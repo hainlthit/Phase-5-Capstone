a52db6daa837cb8db8528a67ec77a89cbd16c0cb
Hain Thit <hainlthit@gmail.com>
2022-03-16 16:07:06 -0400

    cleaned up homepage

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <h1 style={{ textAlign: "center" }}>
+        When You're Here You're Family...
+      </h1>
+      <img
+        src="https://static1.srcdn.com/wordpress/wp-content/uploads/2020/04/Animal-Crossing-New-Horizons-Scariest-Creepy-Villagers.jpg"
+        alt="home page image"
+      />
+    </>
+  );
 }

7245ac135b3cf5884edb8fed8c32c624517b1dc2
Hain Thit <hainlthit@gmail.com>
2022-03-16 19:35:15 -0400

    form data in state

--- client/src/Components/Villagers/NewVillagerForm.jsx
+++ client/src/Components/Villagers/NewVillagerForm.jsx
@@ -1,9 +1,99 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function NewVillagerForm() {
+  const [newName, setNewName] = useState("");
+  const [newSpecies, setNewSpecies] = useState("");
+  const [newBirthday, setNewBirthday] = useState("");
+  const [newPersonality, setNewPersonality] = useState("");
+  const [newImage, setNewImage] = useState("");
 
-    
+  function handleSetNewName(e) {
+    setNewName(e.target.value);
+  }
+
+  function handleSetNewSpecies(e) {
+    setNewSpecies(e.target.value);
+  }
+
+  function handleSetNewBirthday(e) {
+    setNewBirthday(e.target.value);
+  }
+
+  function handleSetNewPersonality(e) {
+    setNewPersonality(e.target.value);
+  }
+
+  function handleSetNewImage(e) {
+    setNewImage(e.target.value);
+  }
+
   return (
-    <h1 style={{ textAlign: "center" }}>NewVillagerForm</h1>
-  )
+    <>
+      <img
+        class="img-fluid"
+        src="https://cdn.mos.cms.futurecdn.net/Dr5tJTKgmVbckAUXFKMeCJ.jpg"
+        alt="new villager form image"
+      />
+
+      <h1 style={{ textAlign: "center" }}>NewVillagerForm</h1>
+
+      <form>
+        <div class="form-group">
+          <label for="exampleFormControlInput1">Name</label>
+          <input
+            class="form-control"
+            type="string"
+            id="exampleFormControlInput1"
+            placeholder="Your new Friend's Name"
+            value={newName}
+            onChange={handleSetNewName}
+          />
+        </div>
+        <div class="form-group">
+          <label for="exampleFormControlInput2">Birthday</label>
+          <input
+            class="form-control"
+            type="string"
+            id="exampleFormControlInput2"
+            placeholder="What is their Birthday?"
+            value={newBirthday}
+            onChange={handleSetNewBirthday}
+          />
+        </div>
+        <div class="form-group">
+          <label for="exampleFormControlInput3">Personality</label>
+          <input
+            class="form-control"
+            type="string"
+            id="exampleFormControlInput3"
+            placeholder="Main Personality Trait"
+            value={newPersonality}
+            onChange={handleSetNewPersonality}
+          />
+        </div>
+        <div class="form-group">
+          <label for="exampleFormControlInput4">Species</label>
+          <input
+            class="form-control"
+            type="string"
+            id="exampleFormControlInput4"
+            placeholder="Identity"
+            value={newSpecies}
+            onChange={handleSetNewSpecies}
+          />
+        </div>
+        <div class="form-group">
+          <label for="exampleFormControlInput5">Image Link</label>
+          <input
+            class="form-control"
+            type="string"
+            id="exampleFormControlInput5"
+            placeholder="Share a pic!"
+            value={newImage}
+            onChange={handleSetNewImage}
+          />
+        </div>
+      </form>
+    </>
+  );
 }
--- client/src/Components/Villagers/VillagerList.jsx
+++ client/src/Components/Villagers/VillagerList.jsx
@@ -7,8 +7,6 @@ export default function VillagerList({ data , userVillagers }) {
   
   const [flip, setFlip] = useState(true);
 
-
-
   function flipTrue() {
     setFlip(true);
   }

ec1052941c33031704792b1839704ba284c5251d
Hain Thit <hainlthit@gmail.com>
2022-03-29 00:06:04 -0400

    working on island limits

--- app/models/island.rb
+++ app/models/island.rb
@@ -1,8 +1,17 @@
 class Island < ApplicationRecord
+  MAX_VILLAGERS_PER_ISLAND = 8
+
   belongs_to :user
 
-  has_many :visitors, dependent: :destroy
-  has_many :villagers, through: :visitors
+  has_many :visitors,  dependent: :destroy
+  has_many :villagers, through: :visitors, before_add: :check_villagers_limit
 
   validates :user_id, uniqueness: true
+
+  private
+
+  def check_villagers_limit(_villager)
+    raise VillagerLimitExceeded if villager.size >= MAX_VILLAGERS_PER_ISLAND
+  end
+
 end
